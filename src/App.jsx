36f9e8c680686342a6df3a69d1b8d99340adcd1c
yongZhang <kinmer7@gmail.com>
2024-05-15 00:01:45 -0400

    add members route in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Clubs from './pages/Clubs';
+import Members from './pages/Members';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -16,6 +17,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/clubs" element={<Clubs />} />
+                    <Route path="/members" element={<Members />} />
                 </Routes>
             </main>
             <Footer />
