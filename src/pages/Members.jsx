e74f7474b996f0cabafa1bdb607a1b2fab03fab1
yongZhang <kinmer7@gmail.com>
2024-05-14 23:59:03 -0400

    add members page

---
+++ src/pages/Members.jsx
@@ -0,0 +1,11 @@
+import MembersList from '../features/members/MembersList';
+
+const Clubs = () => {
+    return (
+        <div>
+            <MembersList />
+        </div>
+    );
+};
+
+export default Clubs;
