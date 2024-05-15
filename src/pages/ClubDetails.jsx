3ba1d82376cedf79fd6c2d62718fe8ee194caf3c
yongZhang <kinmer7@gmail.com>
2024-05-15 14:13:19 -0400

    set up club detail page

--- src/pages/ClubDetails.jsx
+++ src/pages/ClubDetails.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import ClubCard from '../features/clubs/ClubCard';
+
 const ClubDetails = () => {
-    return <div></div>;
+    const [clubDetails, setClubDetails] = useState({});
+    const { _id } = useParams();
+
+    const fetchClubDetails = async () => {
+        let response = await axios.get(`http://localhost:3000/clubs/${_id}?`);
+        console.log(response);
+        setClubDetails(response.data);
+    };
+    useEffect(() => {
+        fetchClubDetails();
+    }, [_id]);
+
+    return (
+        <div className="club-details">
+            <ClubCard name={clubDetails.name} image={clubDetails.image} />
+        </div>
+    );
 };
 
 export default ClubDetails;
