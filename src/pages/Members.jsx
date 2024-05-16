af6bcc5ec70709e0b75e04348dc4ba1a50c1ad3f
yongZhang <kinmer7@gmail.com>
2024-05-16 10:51:59 -0400

    delete member list

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,10 +1,43 @@
-import MembersList from '../features/members/MembersList';
+import {
+    Card,
+    CardBody,
+    CardImg,
+    CardTitle,
+    Row,
+    Col,
+    CardLink,
+    CardText,
+    ListGroup,
+    ListGroupItem,
+} from 'reactstrap';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const Clubs = () => {
+    const [members, setMembers] = useState([]);
+    const fetchClubs = async () => {
+        let response = await axios.get('http://localhost:3000/members');
+        setMembers(response.data);
+    };
+    useEffect(() => {
+        fetchClubs();
+    }, []);
     return (
-        <div>
-            <MembersList />
-        </div>
+        <Row>
+            <Col md="8">
+                <div>
+                    {members.map((member) => (
+                        <div key={member._id}>
+                            <h2>{member.name}</h2>
+                            <h3>{member.age}</h3>
+                            <h3>{member.occupation}</h3>
+                            <p>{member.profile}</p>
+                        </div>
+                    ))}
+                </div>
+            </Col>
+        </Row>
     );
 };
 
