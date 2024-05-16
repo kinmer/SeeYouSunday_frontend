3ff3f8f79d9ee0b2c4471761476784fac4c2d3e4
yongZhang <kinmer7@gmail.com>
2024-05-16 08:30:03 -0400

    add detail info in detail page

--- src/pages/ClubDetails.jsx
+++ src/pages/ClubDetails.jsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import {
+    Card,
+    CardBody,
+    CardSubtitle,
+    CardImg,
+    CardTitle,
+    Row,
+    Col,
+    CardLink,
+    CardText,
+    ListGroup,
+    ListGroupItem,
+} from 'reactstrap';
 import ClubCard from '../features/clubs/ClubCard';
 
 const ClubDetails = () => {
@@ -17,10 +30,29 @@ const ClubDetails = () => {
     }, [_id]);
 
     return (
-        <div className="club-details">
-            <ClubCard name={clubDetails.name} image={clubDetails.image} />
-        </div>
+        <Row>
+            <Col md="7">
+                <Card>
+                    <img alt="club photo" src={clubDetails.image} />
+                    <CardBody>
+                        <CardTitle tag="h4">{clubDetails.name}</CardTitle>
+                        <CardText>{clubDetails.description}</CardText>
+                    </CardBody>
+                    <ListGroup flush>
+                        <ListGroupItem>
+                            Location: {clubDetails.location}
+                        </ListGroupItem>
+                        <ListGroupItem>Time: {clubDetails.time}</ListGroupItem>
+                    </ListGroup>
+                </Card>
+            </Col>
+            <Col md="3"></Col>
+        </Row>
     );
 };
 
 export default ClubDetails;
+
+// <div className="club-details">
+//             <ClubCard name={clubDetails.name} image={clubDetails.image} />
+//         </div>
