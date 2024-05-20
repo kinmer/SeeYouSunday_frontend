d4b02ace03425dd5b2562343713a3e681ec704d1
yongZhang <kinmer7@gmail.com>
2024-05-20 14:12:49 -0400

    set toggle for animated

--- src/features/clubs/AnimatedClubCard.jsx
+++ src/features/clubs/AnimatedClubCard.jsx
@@ -2,7 +2,8 @@ import { Card, CardImg, CardTitle } from 'reactstrap';
 import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
-const ClubCard = (props) => {
+const AnimatedClubCard = (props) => {
+    const [toggle, setToggle] = useState(false);
     return (
         <Card>
             <CardImg width="100%" src={props.image} />
@@ -11,4 +12,4 @@ const ClubCard = (props) => {
     );
 };
 
-export default ClubCard;
+export default AnimatedClubCard;
