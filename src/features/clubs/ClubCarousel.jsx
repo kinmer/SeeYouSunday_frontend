6e25d41524b08c53c273366665b825e3cb537ed8
yongZhang <kinmer7@gmail.com>
2024-05-20 14:40:13 -0400

    import dependency

---
+++ src/features/clubs/ClubCarousel.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+    Row,
+    Col,
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const ClubCaraousel = () => {};
+
+export default ClubCaraousel;
